3e0a1ed6c6c98fcabc006ad1f847352f9b7a9b00
sadid369 <sadid.jones@gmail.com>
2024-05-07 21:48:44 +0600

    updated

---
+++ lib/common/widgets/small_box.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class SmallBox extends StatelessWidget {
+  final double width;
+  final double height;
+  final String text;
+  final Color bg;
+  final Color textColor;
+  final bool isBorderRadios;
+  final bool isIcon;
+  final IconData icon;
+
+  const SmallBox({
+    Key? key,
+    required this.width,
+    required this.height,
+    required this.text,
+    required this.bg,
+    required this.textColor,
+    this.isBorderRadios = false,
+    this.isIcon = false,
+    this.icon = Icons.arrow_forward_outlined,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: bg,
+        borderRadius: isBorderRadios ? BorderRadius.circular(10) : null,
+      ),
+      child: isIcon
+          ? Icon(
+              icon,
+              color: Colors.white,
+            )
+          : Text(
+              text,
+              style: TextStyle(
+                color: textColor,
+              ),
+            ),
+    );
+  }
+}
--- lib/features/home/widgets/inspiration.dart
+++ lib/features/home/widgets/inspiration.dart
@@ -1,5 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:furniro/common/utils/app_colors.dart';
+import 'package:furniro/common/utils/app_image.dart';
 import 'package:furniro/common/utils/constants.dart';
+import 'package:furniro/common/widgets/app_button_golden.dart';
+import 'package:furniro/common/widgets/small_box.dart';
 
 class Inspiration extends StatefulWidget {
   const Inspiration({Key? key}) : super(key: key);
@@ -9,29 +16,159 @@ class Inspiration extends StatefulWidget {
 }
 
 class _InspirationState extends State<Inspiration> {
+  List items = [
+    AppImage.homeImage14,
+    AppImage.homeImage11,
+    AppImage.homeImage16,
+    AppImage.homeImage17,
+    AppImage.homeImage19,
+    AppImage.homeImage21,
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       width: w! * 0.7,
       height: h! * .7,
       decoration: BoxDecoration(
-          border: Border.all(
-        color: Colors.black,
-      )),
+        color: AppColors.goldenFCF8F3,
+        border: Border.all(
+          color: Colors.black,
+        ),
+      ),
       child: Row(
         children: [
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 '50+ Beautiful rooms\ninspiration',
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                  color: AppColors.heading3A3A3A,
                 ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Our designer already made a lot of beautiful\nprototipe of rooms that inspire you',
+                style: TextStyle(
+                  fontSize: 12,
+                  // fontWeight: FontWeight.bold,
+                  color: AppColors.heading616161,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              AppButtonGolden(
+                width: 170,
+                height: 40,
+                text: 'Explore More',
+                onTap: () {},
               )
             ],
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            width: 280,
+            height: 580,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.green),
+              image: DecorationImage(
+                image: AssetImage(
+                  AppImage.homeImage13,
+                ),
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  bottom: 20,
+                  left: 20,
+                  child: Container(
+                    width: 150,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.7),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '01 --- Bed Room',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: AppColors.heading616161,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'Inner Peace',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: AppColors.heading3A3A3A,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 68,
+                  bottom: 20,
+                  child: SmallBox(
+                    width: 40,
+                    height: 40,
+                    text: '',
+                    bg: AppColors.goldenB88E2F,
+                    textColor: Colors.white,
+                    isBorderRadios: false,
+                    isIcon: true,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: 350,
+            height: 300,
+            margin: EdgeInsets.only(left: 20),
+            child: CarouselSlider(
+              items: items.map((e) {
+                return Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fitHeight,
+                      image: AssetImage(
+                        e,
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+              options: CarouselOptions(
+                height: 300,
+                // aspectRatio: 16 / 9,
+                viewportFraction: 0.8,
+                initialPage: 0,
+                enableInfiniteScroll: true,
+                reverse: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 3),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enlargeCenterPage: true,
+                enlargeFactor: 0.3,
+                onPageChanged: (index, reason) {},
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
           )
         ],
       ),
